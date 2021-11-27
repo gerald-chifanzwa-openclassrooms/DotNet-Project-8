bc64fbb8856d7dc7c0852cd857b7620e01e44d99
Gerald Chifanzwa <gchifanzwa@zss.co.zw>
2021-11-27 19:52:14 +0200

    tests for users apis

---
+++ Users.Api.Tests/ApplicationFactory.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Users.Api;
+
+public class ApplicationFactory : WebApplicationFactory<UsersDbContext>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        base.ConfigureWebHost(builder);
+        builder.ConfigureTestServices(services =>
+        {
+            var dbOptionsService = services.FirstOrDefault(s => s.ServiceType == typeof(DbContextOptions<UsersDbContext>));
+            if (dbOptionsService != null)
+                services.Remove(dbOptionsService);
+            services.AddDbContext<UsersDbContext>(dbOptions => dbOptions.UseInMemoryDatabase(nameof(UsersDbContext)));
+        });
+    }
+}
+
---
+++ Users.Api.Tests/UsersEndpointsTests.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+using System.Net.Http.Json;
+using FluentAssertions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Users.Api.Tests;
+
+public class UsersEndpointsTests : IClassFixture<ApplicationFactory>
+{
+    private readonly ApplicationFactory _factory;
+
+    public UsersEndpointsTests(ApplicationFactory factory) => _factory = factory;
+
+
+    [Fact]
+    public void GetUsersTests()
+    {
+        var client = _factory.CreateClient();
+
+        var users = client.GetFromJsonAsync<IReadOnlyCollection<UserViewModel>>("/users").GetAwaiter().GetResult();
+
+        users.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void GetUserByIdTests_GivenInvalidUserId_ShouldReturnNotFound()
+    {
+        var client = _factory.CreateClient();
+
+        var response = client.GetAsync("/users/100").GetAwaiter().GetResult();
+
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.NotFound);
+    }
+
+
+    [Fact]
+    public void GetUserByIdTests()
+    {
+        int userId = 0;
+
+        var client = _factory.WithWebHostBuilder(builder =>
+        {
+            builder.Configure(app =>
+            {
+                using var scope = app.ApplicationServices.CreateScope();
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+                ApplicationUser user = new()
+                {
+                    FullName = "Test User",
+                    Email = "test@example.com",
+                    UserName = "test@example.com",
+                    Type = UserType.Patient
+                };
+
+                userManager.CreateAsync(user).GetAwaiter().GetResult();
+
+                userId = user.Id;
+            });
+        }).CreateClient();
+
+        var user = client.GetFromJsonAsync<UserViewModel>($"/users/{userId}").GetAwaiter().GetResult();
+
+        user.Should().NotBeNull();
+        user.FullName.Should().Be("Test User");
+        user.Email.Should().Be("test@example.com");
+    }
+}
